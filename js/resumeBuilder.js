5ac463b4f6056f92826b7a543ee07f8ef80ede7b
Lorenzo Hernandez <lorenzo.hernandezjr@gmail.com>
2016-03-17 21:28:58 -0600

    feat: Add bio, jobs, education, and project sections

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,131 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// Bio Section
+
+var bio = {
+	"name" : "Lorenzo Hernandez Jr.",
+	"role" : "Front-End Developer",
+	"contacts" : {
+		"mobile" : "915-319-1045",
+		"email" : "lorenzo.hernandezjr@gmail.com",
+		"github" : "lhernandez9x",
+		"twitter" : "@lhernandez9x",
+		"location" : "El Paso, TX"
+	},
+	"welcomeMessage" : "Hello, thanks for checking out my online resume.",
+	"skills" : ["HTML", "CSS", "Javascript", "jQuery","Responsive Design", "Photoshop", "and Awesomeness!"],
+	"bioPic" : "images/me.jpg"
+}
+
+// Work Section
+
+var work = {
+	"jobs" : [
+		{
+			"workEmployer" : "Perry's Oilfield Service",
+			"workTitle" : "Pipeline Operator",
+			"workLocation" : "Carlsbad, NM",
+			"workDates" : "2015 - 2016",
+			"workDescription" : "Work on compressors. Work unsupervised and start, stop, run natural gas compressors."
+		},
+		{
+			"workEmployer" : "Perry's Oilfield Service",
+			"workTitle" : "Pipeline Operator",
+			"workLocation" : "Carlsbad, NM",
+			"workDates" : "2015 - 2016",
+			"workDescription" : "Work on compressors. Work unsupervised and start, stop, run natural gas compressors."
+		},
+		{
+			"workEmployer" : "Perry's Oilfield Service",
+			"workTitle" : "Pipeline Operator",
+			"workLocation" : "Carlsbad, NM",
+			"workDates" : "2015 - 2016",
+			"workDescription" : "Work on compressors. Work unsupervised and start, stop, run natural gas compressors."
+		},
+		{
+			"workEmployer" : "Perry's Oilfield Service",
+			"workTitle" : "Pipeline Operator",
+			"workLocation" : "Carlsbad, NM",
+			"workDates" : "2015 - 2016",
+			"workDescription" : "Work on compressors. Work unsupervised and start, stop, run natural gas compressors."
+		},
+		{
+			"workEmployer" : "Perry's Oilfield Service",
+			"workTitle" : "Pipeline Operator",
+			"workLocation" : "Carlsbad, NM",
+			"workDates" : "2015 - 2016",
+			"workDescription" : "Work on compressors. Work unsupervised and start, stop, run natural gas compressors."
+		}
+	]
+}
+
+//Education Section
+
+var education = {
+	"schools" : [
+		{
+			"schoolName" : "The Art Institute of Pittsburgh - Online",
+			"schoolLocation" : "Online - El Paso, TX",
+			"schoolDegree" : "Bachelors of Arts",
+			"schoolMajor" : ["Web Design and Interactive Media"],
+			"schoolDates" : "2011 - 2012"
+		},
+		{
+			"schoolName" : "The Art Institute of Pittsburgh - Online",
+			"schoolLocation" : "Online - El Paso, TX",
+			"schoolDegree" : "Bachelors of Arts",
+			"schoolMajor" : ["Web Design and Interactive Media"],
+			"schoolDates" : "2011 - 2012"
+		}
+	],
+	"onlineCourses" : [
+		{
+			"title" : "UX Fundamentals",
+			"school" : "Aquent Gymnasium",
+			"dates" : "2016",
+			"url" : "https://accredible.com/10130430"
+		},
+		{
+			"title" : "Javascript Basics",
+			"school" : "Udacity",
+			"dates" : "2016",
+			"url" : "https://www.udacity.com/course/javascript-basics--ud804-nd"
+		},
+		{
+			"title" : "",
+			"school" : "Udacity",
+			"dates" : "2016",
+			"url" : "https://www.udacity.com/course/javascript-basics--ud804-nd"
+		},
+		{
+			"title" : "Javascript Basics",
+			"school" : "Udacity",
+			"dates" : "2016",
+			"url" : "https://www.udacity.com/course/javascript-basics--ud804-nd"
+		}
+	]
+}
+
+//Projects Section
+
+var pojects = {
+	"projects" : [
+		{
+			"title" : "Project Title",
+			"dates" : "2016",
+			"description": "This is to describe the Project.",
+			"images" : ["images/fry.jpg", "images/fry.jpg"]
+		},
+		{
+			"title" : "Project Title",
+			"dates" : "2016",
+			"description": "This is to describe the Project.",
+			"images" : ["images/fry.jpg", "images/fry.jpg"]
+		},
+		{
+			"title" : "Project Title",
+			"dates" : "2016",
+			"description": "This is to describe the Project.",
+			"images" : ["images/fry.jpg", "images/fry.jpg"]
+		}
+
+	]
+}
